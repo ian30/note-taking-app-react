05ca7f19d36fa42947370f49f96ec2e202abb7ed
Ian Shnayder <ian.shnayder@hyland.com>
2023-07-16 15:51:37 -0700

    initial commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState('');
+
+  const addNote = () => {
+    if (currentNote.trim() !== '') {
+      setNotes([...notes, currentNote]);
+      setCurrentNote('');
+    }
+  };
+
+  const deleteNote = (index) => {
+    const updatedNotes = [...notes];
+    updatedNotes.splice(index,1);
+    setNotes(updatedNotes)
+  }
+  const exportNote = () => {
+    const divider = "-----------";
+    const text = notes.join(`\n${divider}\n`);
+    const element = document.createElement('a');
+    const file = new Blob([text], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = 'notes.txt';
+    document.body.appendChild(element);
+    element.click();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h2>Note Taking App</h2>
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder='Enter a note' value={currentNote} onChange={(e) => setCurrentNote(e.target.value)} />
+        <div className='input-group-append'>
+          <button className='btn btn-primary' onClick={addNote}>Add Note</button>
+        </div> 
+      </div>
+      <div>
+        <ul className='list-group'>
+          {notes.map((note, index) => (
+            <li className='list-group-item' key={index}>
+              {note}
+              <button className="btn btn-danger btn-sm tiny" onClick={() => deleteNote(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='mt-3'>
+          <button className='btn btn-success' onClick={exportNote}>Export Note</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
